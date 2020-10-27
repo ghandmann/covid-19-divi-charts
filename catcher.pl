24eb4b0443aa0a8e68b1d1aa7464d37171d21286
Sven Eppler <sven.eppler@sodgeit.de>
2020-10-27 10:46:58 +0100

    scripts

---
+++ catcher.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use 5.10.0;
+use DateTime;
+use LWP::Simple;
+use Mojo::UserAgent;
+use Mojo::URL;
+use Mojo::Log;
+
+my $log = Mojo::Log->new(path => "./divi-catcher.log");
+my $ua = Mojo::UserAgent->new();
+
+my $diviBaseUrl = Mojo::URL->new("https://www.divi.de");
+
+my $overviewBase = "https://www.divi.de/divi-intensivregister-tagesreport-archiv-csv?layout=table&start=";
+my $pageOffset = 0;
+
+# only try getting the first 2 pages
+my $maxPageOffset = 40;
+
+while($pageOffset <= $maxPageOffset) {
+    my $pageUrl = $overviewBase . $pageOffset;
+
+    $log->info("Fetching overview page=$pageUrl");
+
+    my $overviewPage = $ua->get($pageUrl)->res->dom;
+
+    my $linkElements = $overviewPage->find("td.edocman-document-title-td a");
+
+    my $linksFoundCount = $linkElements->size;
+
+    $linkElements->each(sub {
+        my $url = $diviBaseUrl->clone->path(shift->attr("href"));
+        $log->info("Found url: $url");
+        my ($date) = ($url =~ /divi-intensivregister-(\d\d\d\d-\d\d-\d\d)/);
+
+        my $targetFile = "./DIVI/${date}.csv";
+
+        if(-f $targetFile) {
+            $log->info("$targetFile already exists. Not re-downloading report from $url");
+        }
+        else {
+            fetchCSV($url, $targetFile);
+        }
+    });
+
+    $pageOffset += 20;
+}
+
+sub fetchCSV {
+    my $url = shift;
+    my $targetFile = shift;
+
+    if($targetFile =~ /2020-04-24/) {
+        # this file has a completely different format, so we skip it.
+        return;
+    }
+
+    $ua->get($url)->res->save_to($targetFile);
+    $log->info("Fetched $url to $targetFile");
+}
---
+++ report-generator.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Text::CSV qw/csv/;
+use feature "say";
+use Mojo::JSON qw/j/;
+use File::Find;
+use Try::Tiny;
+
+
+my $badDate = "2020-04-25";
+my $goodDate = "2020-05-23";
+
+my @total;
+
+my @files = glob("./DIVI/*.csv");
+
+foreach my $file (@files) {
+    try {
+        my $statsData = generateStatsForDate($file);
+
+        push(@total, $statsData);
+    }
+    catch {
+        say "Failed to process $file. Reason: $_";
+    };
+}
+
+say j \@total;
+
+sub formatRate {
+    return sprintf("%0.2f", shift);
+}
+
+sub generateStatsForDate {
+    my $file = shift;
+
+    my ($date) = ($file =~ /(\d\d\d\d\-\d\d-\d\d)/);
+
+    my $data = csv( in => $file, headers => "auto" );
+
+    if($data->[1]->{faelle_covid_aktuell_im_bundesland}) {
+        return
+    }
+
+    my $sumCovidTotal = 0;
+    my $sumCovidVentilated = 0;
+
+    my $sumICUTotal = 0;
+    my $sumICUUsed = 0;
+
+    foreach my $row (@$data) {
+        # bundesland;gemeindeschluessel;anzahl_meldebereiche;faelle_covid_aktuell;faelle_covid_aktuell_beatmet;anzahl_standorte;betten_frei;betten_belegt;daten_stand
+        $sumCovidTotal += $row->{faelle_covid_aktuell};
+        $sumCovidVentilated += $row->{faelle_covid_aktuell_beatmet} || 0;
+
+        $sumICUTotal += $row->{betten_frei} + $row->{betten_belegt};
+        $sumICUUsed += $row->{betten_belegt};
+    }
+
+    return {
+        date => $date,
+        covidPatients => {
+            total => $sumCovidTotal,
+            ventilated => $sumCovidVentilated,
+            ventilationRate => formatRate($sumCovidVentilated / $sumCovidTotal),
+        },
+        ICUBeds => {
+            total => $sumICUTotal,
+            used => $sumICUUsed,
+            free => $sumICUTotal - $sumICUUsed,
+            covidRate => formatRate($sumCovidTotal / $sumICUUsed),
+            freeRate => formatRate($sumICUUsed / $sumICUTotal),
+        }
+    };
+}
